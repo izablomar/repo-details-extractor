c1fef7120f655a336b6baaae51589048425d4e9c
Izabela Blonska-Martyniuk <iblon@softserveinc.com>
2022-11-16 13:05:45 +0100

    project and dependencies initialization

---
+++ src/main/java/com/example/repodetailsextractor/RepoDetailsExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.repodetailsextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoDetailsExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoDetailsExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repodetailsextractor/RepoDetailsExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repodetailsextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoDetailsExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
